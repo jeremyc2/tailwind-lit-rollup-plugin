1c78f2cbf3d9464ec93c2bda367ef02ed98b0e72
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2022-10-08 17:53:08 -0500

    Add files via upload

---
+++ tailwind-plugin.cjs
@@ -0,0 +1,119 @@
+const { types, transformSync } = require("@babel/core");
+const nodePath = require("path");
+
+const { execSync } = require("child_process");
+
+function escapeShellArg(arg) {
+  return `'${arg.replace(/'/g, `'\\''`)}'`;
+}
+
+function getAbsolutePath(path) {
+  let absolutePath = "";
+
+  for (
+    let currentPath = path;
+    currentPath.key !== "program";
+    currentPath = currentPath.parentPath
+  ) {
+    const isIndex = typeof currentPath.key === "number";
+
+    absolutePath = `${isIndex ? `${currentPath.listKey}.` : ""}${
+      currentPath.key
+    }${absolutePath ? `.${absolutePath}` : ""}`;
+  }
+
+  return absolutePath;
+}
+
+function removeCSS(source) {
+  const css = [];
+  const { code } = transformSync(source, {
+    plugins: [
+      function removeCSS() {
+        return {
+          visitor: {
+            TaggedTemplateExpression(path) {
+              if (path.node.tag.name !== "css") return;
+
+              css.push({
+                absolutePath: getAbsolutePath(path),
+                code: path.getSource(),
+              });
+              path.replaceWith(types.stringLiteral("CSS Placeholder"));
+              path.skip();
+            },
+          },
+        };
+      },
+    ],
+  });
+
+  return { code, css };
+}
+
+function addTailwindCSS({ code: source, css }, tailwindConfig = {}) {
+  const { code } = transformSync(source, {
+    plugins: [
+      function addTailwindCSS() {
+        return {
+          visitor: {
+            Program(path) {
+              css.forEach(({ absolutePath, code }) => {
+                const currentPath = path.get(absolutePath);
+
+                tailwindConfig.content = [
+                  {
+                    raw: source,
+                  },
+                ];
+
+                currentPath.replaceWithSourceString(code);
+
+                currentPath.get("quasi.quasis").forEach(async (quasiPath) => {
+                  const value = quasiPath.node.value.cooked;
+
+                  const tailwindScriptPath = nodePath.join(
+                    __dirname,
+                    "scripts/tw.js"
+                  );
+
+                  let css;
+                  try {
+                    css = execSync(
+                      `node "${tailwindScriptPath}" ${escapeShellArg(
+                        JSON.stringify(tailwindConfig)
+                      )} ${escapeShellArg(value)}`
+                    ).toString();
+
+                    css = css
+                      .replaceAll(/^\s*\n/gm, "")
+                      .replaceAll("\\", "\\\\")
+                      .replaceAll("`", "\\`");
+                    quasiPath.replaceWith(types.templateElement({ raw: css }));
+                  } catch (error) {
+                    console.log("Some CSS could not be parsed by TailwindCSS");
+                  }
+                });
+              });
+
+              path.skip();
+            },
+          },
+        };
+      },
+    ],
+  });
+
+  return code;
+}
+
+module.exports = function tailwindPlugin(tailwindConfig) {
+  return {
+    name: "tailwind-plugin",
+    transform(source, id) {
+      if (!id.endsWith(".js") && !id.endsWith(".ts")) return;
+      const result = addTailwindCSS(removeCSS(source), tailwindConfig);
+      return result;
+    },
+  };
+}
